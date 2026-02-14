08f598583b99cbb68de56054b93cda2a626d0b50
Baelhj <syamo.imoo31@gmail.com>
2026-02-14 21:10:22 +0100

    word counter

---
+++ CLI Word Counter/word_counter.py
@@ -0,0 +1,29 @@
+# file input
+file = input("enter the file path: ")
+
+# open file
+try:
+    with open(file, encoding="utf-8") as f:
+        poem = f.read()
+
+        # count words in file
+        words = poem.split()
+        word_count = len(words)
+        print(words)
+        print("Total words is: ", word_count)
+
+        # count unique words in file by removing dublicates
+        unique_words_count = len(set(words))
+        print("Total unique words is: ", unique_words_count)
+
+        # Frequency word counter
+        word_dict = {}
+        for w in words:
+            if w in word_dict:
+                word_dict[w] += 1
+            else:
+                word_dict[w] = 1
+
+        print(word_dict)
+except FileNotFoundError:
+    print("File Not Found bro")
